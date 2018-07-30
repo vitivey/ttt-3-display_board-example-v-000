5933e9638a83eec5226cee9f307cdebc62156c8e
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-07-30 21:02:43 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-    row_1=["   ","|   |","   "]
+    row_1=["   ","   |   |   ","   "]
     separator="-----------"
-    row_2=["   ","|   |","   "]
-    row_3=["   ","|   |","   "]
+    row_2=["   ","   |   |   ","   "]
+    row_3=["   ","   |   |   ","   "]
     puts row_1
     puts separator
     puts row_2
