b7c623bd6d4ffba08a542ae18b0c168675e4cb00
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-07-30 21:24:41 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,11 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+    row=["   |   |   "]
+    separator="-----------"
+    puts row
+    puts separator
+    puts row
+    puts separator
+    puts row
+  end
+  puts display_board
